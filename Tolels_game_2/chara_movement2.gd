fbb991422122abe3999f276d7a749332deb6f480
Sim-Ju <sim90ju@gmail.com>
2025-06-14 18:27:42 +0200

    Chara movement2 funktioniert ist aber kacke

--- Tolels_game_2/chara_movement2.gd
+++ Tolels_game_2/chara_movement2.gd
@@ -1,14 +1,23 @@
 extends CharacterBody2D
 
 var yspeed = 0
+var xspeed = 0
 
 func get_input():
+	if is_on_floor():
+		yspeed = 0
+		xspeed /= 1.2
+	else:
+		xspeed /= 1.01
 	if Input.is_action_just_pressed("Jump"):
-		yspeed == 100
+		yspeed = -300
 	else:
 		yspeed += 10
-	velocity = Vector2(0, yspeed)
-
+	if Input.is_action_pressed("Left"):
+		xspeed -= 10
+	if Input.is_action_pressed("Right"):
+		xspeed += 10
+	velocity = Vector2(xspeed, yspeed)
 
 func _physics_process(delta):
 	get_input()
