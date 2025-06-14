58e784ee123eff0379f3b6b52d0e79423bbe3573
gudischlecht <spamblocke@gmail.com>
2025-06-14 15:19:18 +0200

    janky first platformer movement prototype

--- Tolels_game_2/chara_movement.gd
+++ Tolels_game_2/chara_movement.gd
@@ -1,10 +1,24 @@
 extends CharacterBody2D
 
 @export var speed = 400
+@export var gravity = Vector2(0, 200)
+var jumpheight = 30
 
 func get_input():
-	var input_direction = Input.get_vector("Left", "Right", "Jump", "Down")
-	velocity = input_direction * speed
+	if jumpheight > 0:
+		var input_direction = Input.get_vector("Left", "Right", "Jump", "Down")
+		#velocity = input_direction * speed + gravity
+		velocity = velocity.move_toward(input_direction * speed + gravity, 150)
+			
+	else:
+		var input_direction = Input.get_vector("Left", "Right", "", "")
+		#velocity = input_direction * speed + gravity
+		velocity = velocity.move_toward(input_direction * speed + gravity, 100)
+	if is_on_floor():
+		jumpheight = 30
+	else:
+		jumpheight -= 1
+	
 
 func _physics_process(delta):
 	get_input()
