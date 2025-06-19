89afebf23f6f9c38478b1f6fa07b2b5583af783e
espenk-tech <espen.knapp@gmail.com>
2025-06-19 02:29:09 +0200

    cam on timer now

--- Tolels_game_2/camera_2d.gd
+++ Tolels_game_2/camera_2d.gd
@@ -2,8 +2,12 @@ extends Camera2D
 
 @export var return_speed: float = 5.0
 
-var freeze_camera = false
+@onready var player = find_node_recursive(get_tree().get_current_scene(), "CharacterBody2D")
+@onready var area   = find_node_recursive(get_tree().get_current_scene(), "use_cam_area")
+
+var freeze_camera   = false
 var frozen_position = Vector2.ZERO
+var freeze_timer_running = false
 
 func find_node_recursive(node: Node, name: String) -> Node:
 	if node.name == name:
@@ -14,12 +18,11 @@ func find_node_recursive(node: Node, name: String) -> Node:
 			return found
 	return null
 
-@onready var player = find_node_recursive(get_tree().get_current_scene(), "CharacterBody2D")
-
 func _process(delta):
 	if player == null:
 		push_warning("CharacterBody2D node not found!")
 		return
+
 	if freeze_camera:
 		global_position = global_position.lerp(frozen_position, delta * return_speed)
 	else:
@@ -27,12 +30,20 @@ func _process(delta):
 
 func _input(event):
 	if event.is_action_pressed("follow_stop"):
-		if not freeze_camera:
-			freeze_camera = true
-			frozen_position = player.global_position
+		var in_area = area.get_overlapping_bodies().has(player)
+		if in_area and not freeze_timer_running:
+			start_freeze()
 	elif event.is_action_released("follow_stop"):
-		freeze_camera = false
-		
-	var win := get_window()
-	var parent_win := get_parent().get_window()
-	var current_size := win.size
+		pass
+
+func start_freeze():
+	freeze_camera = true
+	freeze_timer_running = true
+	frozen_position = player.global_position
+	print("10 Sekunden")
+
+	await get_tree().create_timer(10.0).timeout
+
+	freeze_camera = false
+	freeze_timer_running = false
+	print("▶️ Kamera folgt wieder")
